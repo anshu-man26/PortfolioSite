f53b2eea93d98a0ac64fb5345a874da502b4042f
AnshumanSingh2600 <122178537+AnshumanSingh2600@users.noreply.github.com>
2025-11-13 21:17:50 +0530

    check

--- backend/server.js
+++ backend/server.js
@@ -46,6 +46,4 @@ app.use('/images', express.static('public/images'));
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Portfolio server running on port ${PORT}`);
-}); 
+app.listen(PORT, "0.0.0.0", () => console.log("Server running"));
